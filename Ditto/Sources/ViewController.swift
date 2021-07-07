f7e91875ace73b60448458697dab0858966bbfca
Daniel Okada <dtmokada@gmail.com>
2021-07-07 14:04:40 -0300

    Add main project

---
+++ Ditto/Sources/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Ditto
+//
+//  Created by Daniel Okada on 01/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
+

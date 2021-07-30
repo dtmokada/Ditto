56f38a9858c8ee13afa9f7558c1fa0bb7566fe3e
Daniel Okada <dtmokada@gmail.com>
2021-07-29 23:00:17 -0300

    Add NetworkingAPI module

---
+++ Libraries/NetworkingAPI/Sources/Provider/NWProvider.swift
@@ -0,0 +1,26 @@
+//
+//  NWProvider.swift
+//  NetworkingAPI
+//
+//  Created by Daniel Okada on 12/07/21.
+//
+
+import Foundation
+import UIKit
+
+public protocol NWProviderProtocol {
+    var callbackQueue: DispatchQueue { get set }
+    @discardableResult func request<T: Decodable>(_ request: NWRequest, completion: @escaping NWResult<T>) -> Cancellable
+    @discardableResult func request<T: Decodable>(_ request: NWRequest, decoder: JSONDecoder, completion: @escaping NWResult<T>) -> Cancellable
+    @discardableResult func requestImage(url: URL, completion: @escaping NWResult<UIImage>) -> Cancellable
+}
+
+public extension NWProviderProtocol {
+
+    func request<T: Decodable>(_ request: NWRequest, completion: @escaping NWResult<T>)  -> Cancellable {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return self.request(request, decoder: decoder, completion: completion)
+    }
+
+}

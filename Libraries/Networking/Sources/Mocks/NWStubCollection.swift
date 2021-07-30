9a705f8d09196cb9dae25b6807ab7a5298bcefe4
Daniel Okada <dtmokada@gmail.com>
2021-07-30 18:41:59 -0300

    Add stub responses

---
+++ Libraries/Networking/Sources/Mocks/NWStubCollection.swift
@@ -0,0 +1,17 @@
+//
+//  NWStubCollection.swift
+//  Networking
+//
+//  Created by Daniel Okada on 30/07/21.
+//
+
+import Foundation
+
+public protocol NWStubCollection {
+    static var bundle: Bundle { get }
+    static var stubs: [(urlRegex: String, jsonName: String)] { get }
+}
+
+public extension NWStubCollection {
+    static var bundle: Bundle { .main }
+}
---
+++ Libraries/Networking/Sources/Mocks/NWStubs.swift
@@ -0,0 +1,57 @@
+//
+//  NWStubs.swift
+//  Networking
+//
+//  Created by Daniel Okada on 30/07/21.
+//
+
+import Foundation
+
+public class NWStubs {
+
+    public static var shared = NWStubs()
+
+    public var responseDelay: TimeInterval
+
+    private(set) var stubs: [String: URL]
+
+    init(responseDelay: TimeInterval = 0, stubs: [String: URL] = [:]) {
+        self.responseDelay = responseDelay
+        self.stubs = stubs
+    }
+
+    public func register(stubCollection: NWStubCollection.Type) {
+        stubCollection.stubs.forEach { (urlRegex, jsonName) in
+            register(urlRegex: urlRegex, jsonName: jsonName, inBundle: stubCollection.bundle)
+        }
+    }
+
+    @discardableResult
+    public func register(urlRegex: String, jsonName: String, inBundle bundle: Bundle) -> Bool {
+        guard let path = bundle.path(forResource: jsonName, ofType: "json") else { return false }
+        stubs[urlRegex] = URL(fileURLWithPath: path)
+        return true
+    }
+
+    public func removeStubFor(urlRegex: String) {
+        stubs.removeValue(forKey: urlRegex)
+    }
+
+    public func removeAllStubs() {
+        stubs.removeAll()
+    }
+
+    public func dataFor(request: URLRequest) -> Data? {
+        let url = request.url?.absoluteString ?? ""
+        let firstMatch = stubs.keys.first { regex in
+            let range = url.range(of: regex, options: .regularExpression)
+            return range?.lowerBound == url.startIndex && range?.upperBound == url.endIndex
+        }
+        guard let key = firstMatch,
+              let fileUrl = stubs[key],
+              let data = try? Data(contentsOf: fileUrl)
+            else { return nil }
+        return data
+    }
+
+}

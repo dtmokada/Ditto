922244b213bf7840fcbf1cf02ae6315cad300a44
Daniel Okada <dtmokada@gmail.com>
2021-07-29 23:04:57 -0300

    Add Networking module

---
+++ Libraries/Networking/Sources/Mocks/DummyCancellable.swift
@@ -0,0 +1,12 @@
+//
+//  DummyCancellable.swift
+//  Networking
+//
+//  Created by Daniel Okada on 29/07/21.
+//
+
+import NetworkingAPI
+
+struct DummyCancellable: Cancellable {
+    func cancel() {}
+}

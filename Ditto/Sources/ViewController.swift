d654c16030755e7fba945d45b83b76ec44d13e6a
Daniel Okada <dtmokada@gmail.com>
2021-08-03 21:16:12 -0300

    Update main ViewController's layout

--- Ditto/Sources/ViewController.swift
+++ Ditto/Sources/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
 
     let routerService: RouterServiceProtocol
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Main App"
+        label.font = .preferredFont(forTextStyle: .largeTitle, compatibleWith: nil)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Start XKCD module", for: .normal)
+        button.addTarget(self, action: #selector(routeToXkcd), for: .touchUpInside)
+        return button
+    }()
+
     init(routerService: RouterServiceProtocol) {
         self.routerService = routerService
         super.init(nibName: nil, bundle: nil)
@@ -24,7 +40,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+
+        view.addSubview(view: titleLabel, constraints: [
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
+        ])
+
+        view.addSubview(view: button, constraints: [
+            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64),
+        ])
     }
 
     @objc private func routeToXkcd() {

90a2192cfd060e081840487647974a32879f437f
Daniel Okada <dtmokada@gmail.com>
2021-08-03 21:02:40 -0300

    Add xkcd demo's DependencyRegistration

---
+++ Libraries/XKCD/Demo/DependencyRegistration.swift
@@ -0,0 +1,35 @@
+//
+//  DependencyRegistration.swift
+//  DemoApp
+//
+//  Created by Daniel Okada on 03/08/21.
+//
+
+import UIKit
+import Utilities
+import NetworkingAPI
+import Networking
+import NetworkingMock
+import RouterServiceInterface
+import XKCD
+
+class DependencyRegistration: DependencyRegistrationProtocol {
+
+    static func registerRouteHandlers(on routerService: RouterServiceRegistrationProtocol) {
+        routerService.register(routeHandler: XKCDRouteHandler())
+    }
+
+    static func registerDependencyFactories(on routerService: RouterServiceRegistrationProtocol) {
+        routerService.register(dependencyFactory: {
+            let provider = NWProvider(urlSession: .mock)
+            return provider
+        }, forType: NWProviderProtocol.self)
+    }
+
+    static func registerStubs() {
+        UIImageView.urlSession = .mock
+        NWStubs.shared.responseDelay = 1
+        NWStubs.shared.register(stubCollection: ComicStubCollection.self)
+    }
+
+}
